0d8132373808416ac2b4d1a2fbca795e1e9d24b5
Bito <tyrion72@gmail.com>
2024-08-09 10:56:45 +0200

    Initial Commit

---
+++ MovieFlick/MovieFlickApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlickApp.swift
+//  MovieFlick
+//
+//  Created by Alberto Alegre Bravo on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
