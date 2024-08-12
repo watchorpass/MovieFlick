ea396002404db984264c8b3dd1b701474a74313a
Fran O <contact@fran-ochoa.es>
2024-08-12 17:22:16 +0200

    Added cellSize Gradient for title visualization Text modifiers for visualization improvement.

--- MovieFlick/GridCellComponent.swift
+++ MovieFlick/GridCellComponent.swift
@@ -8,20 +8,47 @@
 import SwiftUI
 
 struct GridCellComponent: View {
-    let title: String = "TITULO"
+    let title: String
     let image: Image? = nil
+    let cellSize: CGFloat
     var action: () -> Void
     
+    var gradient = LinearGradient(
+        colors: [.black.opacity(0.8), .black.opacity(0)],
+        startPoint: .bottom,
+        endPoint: .center
+    )
+    
     var body: some View {
-        ZStack() {
+        ZStack(alignment: .bottomLeading) {
             (image ?? Image(systemName: "popcorn"))
                 .resizable()
                 .scaledToFit()
+                .padding()
+                .frame(width: cellSize, height: cellSize)
+                .background(.ultraThinMaterial) // por si sale el placeholder popcorn, que se vea el fondo difuminado.
+            gradient
             Text(title)
+                .fontWeight(.light)
+                .foregroundColor(.white)
+                .lineLimit(1)
+                .padding(8)
+                .allowsTightening(true)
+                .truncationMode(.middle) // si una pelicula es la 2 o el final del título es lo importante. De esta manera se permite leer correcetamente.
         }
+        .clipShape(RoundedRectangle(cornerRadius: cellSize/15))
+        .frame(width: cellSize, height: cellSize)
     }
 }
 
-#Preview {
-    GridCellComponent(action: {})
+#Preview("large") {
+    GridCellComponent(title: "Movie title",cellSize: 300, action: {})
+}
+
+#Preview("medium") {
+    GridCellComponent(title: "Movie title",cellSize: 200, action: {})
+}
+
+#Preview("small") {
+    GridCellComponent(title: "Movie title",cellSize: 150, action: {})
 }
