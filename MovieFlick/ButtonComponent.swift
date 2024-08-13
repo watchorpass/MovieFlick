52a172fe8677b9316dd9b49a74c97ee00181fe65
franmu94 <fmalourarte@gmail.com>
2024-08-13 09:54:37 +0200

    Archivo creado

---
+++ MovieFlick/ButtonComponent.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonComponent.swift
+//  MovieFlick
+//
+//  Created by Fran Malo on 13/8/24.
+//
+
+import SwiftUI
+
+struct ButtonComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonComponent()
+}
