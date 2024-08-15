637177468d17522f3ed73a99c8b1afe5bdd77a8f
franmu94 <fmalourarte@gmail.com>
2024-08-15 20:05:51 +0200

    Componente Boton

--- MovieFlick/ButtonComponent.swift
+++ MovieFlick/ButtonComponent.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct ButtonComponent: View {
+    var title: String
+    var action: () -> Void
+    var color: Color = .yellow
+    
+    @State private var isPressed: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            action()
+        }) {
+            Text(title)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+                .padding()
+                .frame(maxWidth: 250, maxHeight: 50)
+                .background(isPressed ? color.opacity(0.4) : color.opacity(0.5))
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 4)
+                        .fill(color)
+                }
+                .scaleEffect(isPressed ? 0.95 : 1.0) // Leve reducción al presionar
+
+        }
+        .simultaneousGesture(
+            DragGesture(minimumDistance: 0)
+                .onChanged { _ in
+                    withAnimation(.easeInOut(duration: 0.1)) {
+                        isPressed = true
+                    }
+                }
+                .onEnded { _ in
+                    withAnimation(.easeInOut(duration: 0.1)) {
+                        isPressed = false
+                    }
+                }
+        )
     }
 }
-
 #Preview {
-    ButtonComponent()
+    ButtonComponent(title: "Popular") {
+        print("Popular button tapped")
+    }
 }
