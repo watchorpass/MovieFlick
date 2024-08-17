02a59fc8bccabbecc891fcfe041cfdf6f6932306
alex <12alexmaxu@gmail.com>
2024-08-17 18:56:07 +0200

    playersView

--- MovieFlick/PlayersView.swift
+++ MovieFlick/PlayersView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct PlayersView: View {
+    @State var textExample = ["Player 1", "Player 2"]
     var body: some View {
         VStack {
             Text("Create your players")
+                .font(.title)
+                .bold()
             Spacer()
-            ButtonComponent(title: "Add new player", action: {}, color: Color.green)
+            ForEach ($textExample, id: \.self) { player in
+                ZStack {
+                    ButtonComponent(title: "", action: {}, color: Color.green)
+                    TextField("test1", text: player)
+                        .font(.title)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .frame(width: 200)
+                }
+            }
+            ButtonComponent(title: "Add new player +", action: {}, color: Color.yellow)
             Spacer()
-            ButtonComponent(title: "Continue", action: { })
+            ButtonComponent(title: "Continue", action: {})
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background {
