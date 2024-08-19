5462af5492b02929b237ffc8b055afcf3b3b4ca3
alex <12alexmaxu@gmail.com>
2024-08-19 22:18:08 +0200

    Cleaned "PlayersView" good

--- MovieFlick/PlayersView.swift
+++ MovieFlick/PlayersView.swift
@@ -10,26 +10,45 @@ import SwiftUI
 struct PlayersView: View {
 
     @State var textExample = ["", ""]
-
+    var addPlayerText: String {
+        if textExample.count < 4 {
+            return "Add new player +"
+        } else {
+            return "Max. 4 players"
+        }
+    }
     var body: some View {
         VStack {
             Text("Create your players")
                 .font(.title)
                 .bold()
                 .foregroundStyle(Color.yellow)
-
             Spacer()
             ForEach(textExample.indices, id: \.self) { index in
-                ZStack {
-                    ButtonComponent(title: "", action: {}, color: Color.green)
-                    TextField("Player \(index + 1)", text: $textExample[index])
-                        .font(.title)
-                        .bold()
-                        .multilineTextAlignment(.center)
-                        .frame(width: 200)
+                HStack{
+                    ZStack {
+                        ButtonComponent(title: "", action: {}, color: Color.green)
+                        TextField("Player \(index + 1)", text: $textExample[index])
+                            .font(.title)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .frame(width: 200)
+                    }
+                    ButtonComponent(title: "-",
+                                    action: {
+                                    textExample.remove(at: index)
+                                    },
+                                    color: .red)
+                        .frame(width: 50)
                 }
             }
-            ButtonComponent(title: "Add new player +", action: {textExample.append("")}, color: Color.yellow)
+            ButtonComponent(title: addPlayerText,
+                            action: {
+                                if textExample.count < 4 {
+                                    textExample.append("")
+                                }
+                            },
+                            color: Color.yellow)
             Spacer()
             ButtonComponent(title: "Continue", action: {})
         }
