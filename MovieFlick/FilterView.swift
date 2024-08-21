afde2ac7a1b82255613df162537279e5e5e110cb
Fran O <contact@fran-ochoa.es>
2024-08-21 11:46:12 +0200

    Now fits properly in any iOS device (SE, plus and PRO MAX) GeometryReader FTW! ScrollView

--- MovieFlick/FilterView.swift
+++ MovieFlick/FilterView.swift
@@ -4,22 +4,26 @@ struct FilterView: View {
     let selectedType: String
     
     var body: some View {
-        VStack {
-            Text(selectedType.uppercased())
-                .font(.title)
-                .bold()
-                .foregroundStyle(.yellow)
-            Text("Select which list you want to see")
-                .foregroundStyle(.yellow)
-            Spacer()
-            GridCellComponent(title: "Popular", cellSize: 300, image: Image(.interestellar), action: {})
-                .padding()
-            GridCellComponent(title: "Top rated", cellSize: 300, image: Image(.theLastOfUs), action: {})
-                .padding()
-            Spacer()
+        GeometryReader { geometry in
+            VStack {
+                Text(selectedType.uppercased())
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.yellow)
+                Text("Select which list you want to see")
+                    .foregroundStyle(.yellow)
+                Spacer(minLength: 10)
+                ScrollView {
+                    GridCellComponent(title: "Popular", cellSize: geometry.size.width*0.8, image: Image(.interestellar), action: {})
+                        .padding()
+                    GridCellComponent(title: "Top rated", cellSize: geometry.size.width*0.8, image: Image(.theLastOfUs), action: {})
+                        .padding()
+                    Spacer()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .appBackground()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .appBackground()
     }
 }
 
--- MovieFlick/GenreView.swift
+++ MovieFlick/GenreView.swift
@@ -17,10 +17,16 @@ struct GenreView: View {
                 .foregroundStyle(.yellow)
             Text("Select your favourite genres")
                 .foregroundStyle(.yellow)
-            LazyVGrid(columns: [GridItem(), GridItem()], spacing: 40) {
-                ForEach(genreList) { genres in
-                    GridCellComponent(title: genres.name, cellSize: 160, color: .indigo, action: {})
+            Spacer()
+            GeometryReader { geometry in
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(), GridItem()], spacing: 20) {
+                        ForEach(genreList) { genres in
+                            GridCellComponent(title: genres.name, cellSize: geometry.size.width*0.4, color: .teal, action: {})
+                        }
+                    }
                 }
+                .padding(.horizontal)
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -29,5 +35,5 @@ struct GenreView: View {
 }
 
 #Preview {
-    GenreView(filteredType: "Popular", genreList: [Genre(name: "Action", id: 1), Genre(name: "Comedy", id: 2), Genre(name: "Thriller", id: 3), Genre(name: "Romantic", id: 4), Genre(name: "Drama", id: 5)])
+    GenreView(filteredType: "Popular", genreList: [Genre(name: "Action", id: 1), Genre(name: "Comedy", id: 2), Genre(name: "Thriller", id: 3), Genre(name: "Romantic", id: 4), Genre(name: "Drama", id: 5), Genre(name: "Drama 6", id: 6), Genre(name: "Drama 7", id: 7), Genre(name: "Drama 8", id: 8), Genre(name: "Drama 9", id: 9)])
 }
