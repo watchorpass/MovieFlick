87a90458ae81244a794a4d642618565af7a68926
alex <12alexmaxu@gmail.com>
2024-08-21 09:54:31 +0200

    Refactor textExample from "PlayersView"

--- MovieFlick/PlayersView.swift
+++ MovieFlick/PlayersView.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct PlayersView: View {
 
-    @State var textExample = ["", ""]
+    @State var PlayersName = ["", ""]
     var addPlayerText: String {
-        if textExample.count < 4 {
+        if PlayersName.count < 4 {
             return "Add new player +"
         } else {
             return "Max. 4 players"
@@ -24,20 +24,20 @@ struct PlayersView: View {
                 .bold()
                 .foregroundStyle(Color.yellow)
             Spacer()
-            ForEach(textExample.indices, id: \.self) { index in
+            ForEach(PlayersName.indices, id: \.self) { index in
                 HStack{
                     ZStack {
                         ButtonComponent(title: "", action: {}, color: Color.green)
-                        TextField("Player \(index + 1)", text: $textExample[index])
+                        TextField("Player \(index + 1)", text: $PlayersName[index])
                             .font(.title)
                             .bold()
                             .multilineTextAlignment(.center)
                             .frame(width: 200)
                     }
-                    if textExample.count > 2 {
+                    if PlayersName.count > 2 {
                         ButtonComponent(title: "-",
                                         action: {
-                            textExample.remove(at: index)
+                            PlayersName.remove(at: index)
                         },
                                         color: .red)
                         .frame(width: 50)
@@ -46,8 +46,8 @@ struct PlayersView: View {
             }
             ButtonComponent(title: addPlayerText,
                             action: {
-                                if textExample.count < 4 {
-                                    textExample.append("")
+                                if PlayersName.count < 4 {
+                                    PlayersName.append("")
                                 }
                             },
                             color: Color.yellow)
