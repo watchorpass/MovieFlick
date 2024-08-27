eb1863745d1e3d15731b3c6670c511a391f4d8b8
Bito <tyrion72@gmail.com>
2024-08-27 16:48:07 +0200

    BackButton and some resizes

---
+++ MovieFlick/BackButtonComponent.swift
@@ -0,0 +1,28 @@
+//
+//  BackButtonComponent.swift
+//  MovieFlick
+//
+//  Created by Alberto Alegre Bravo on 27/8/24.
+//
+
+import SwiftUI
+
+struct BackButtonComponent: View {
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Image(systemName: "chevron.left")
+                .font(.title2)
+                .fontWeight(.medium)
+                .foregroundStyle(.yellow)
+        }
+
+    }
+}
+
+#Preview {
+    BackButtonComponent() {}
+}
