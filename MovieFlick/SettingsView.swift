0215a4afa1ed40798fcef4d54d4aa8de4c14cb5f
Fran O <contact@fran-ochoa.es>
2024-09-27 16:30:43 +0200

    some visual tweaks

--- MovieFlick/SettingsView.swift
+++ MovieFlick/SettingsView.swift
@@ -7,49 +7,51 @@ struct SettingsView: View {
     
     var body: some View {
         @Bindable var bvm = vm
-        HStack{
-            Spacer()
+        
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("⚙️ Settings")
+                    .font(.largeTitle)
+                    .padding(.top)
+                DisclosureGroup("Platforms allowed", isExpanded: $isExpanded) {
+                    VStack {
+                        ForEach(Provider.avaibleProviders, id: \.self) { provider in
+                            Toggle("\(provider)", isOn: Binding(get: {
+                                vm.isProviderSelected(provider: provider)
+                            }, set: { isOn in
+                                vm.toggleProvider(provider: provider)
+                            }))
+                            .padding(.horizontal)
+                            
+                        }
+                    }
+                }
+                .tint(.primary)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 20)
+                    .fill(.ultraThinMaterial))
+                .padding()
+                Spacer()
+                Text("`Made with ♡ by Alberto, Alex, Fran M., and Fran O.`")
+                    .font(.caption)
+            }
+        }
+        .overlay(alignment: .topTrailing) {
             Button{
                 dismiss()
             } label: {
                 Image(systemName: "xmark.circle.fill")
                     .font(.title)
                     .foregroundStyle(.regularMaterial)
-                
             }
-            .padding()
+            .padding(5)
+            
         }
-        VStack(spacing: 20) {
-            Image(systemName: "gear")
-            Text("⚙️ Settings")
-                .font(.largeTitle)
-            DisclosureGroup("Platforms allowed", isExpanded: $isExpanded) {
-                VStack {
-                    ForEach(Provider.avaibleProviders, id: \.self) { provider in
-                        Toggle("\(provider)", isOn: Binding(get: {
-                            vm.isProviderSelected(provider: provider)
-                        }, set: { isOn in
-                            vm.toggleProvider(provider: provider)
-                        }))
-                        .padding(.horizontal)
-                        
-                    }
-                }
-            }
-            .tint(.primary)
-            .padding()
-            .background(RoundedRectangle(cornerRadius: 20)
-                .fill(.ultraThinMaterial))
-            .padding()
-            Spacer()
-            Text("`Made with ♡ by Alberto, Alex, Fran M., and Fran O.`")
-                .font(.caption)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            Color.purple.opacity(0.8)
+                .ignoresSafeArea()
         }
-        //        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        //        .background {
-        //            Color.purple.opacity(0.8)
-        //                .ignoresSafeArea()
-        //        }
     }
 }
 
